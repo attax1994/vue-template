10e244f17074391061bcf20479b58e8144ea8359
Leeyy <965522899@qq.com>
2018-05-14 14:49:15 +0800

    新增登录功能

--- src/components/home/NavBar/NavBar.vue
+++ src/components/home/NavBar/NavBar.vue
@@ -26,49 +26,73 @@
         </div>
 
         <div class="right" id="app">
-          <el-button type="success">微信登陆</el-button>
 
-          <div class="after-login">
-            <el-dropdown style="margin-right:20px;">
-              <span class="el-dropdown-link">
-                讲师<i class="el-icon-arrow-down el-icon--right"></i>
-              </span>
-              <el-dropdown-menu slot="dropdown" style="margin-right:10px;">
-                <el-dropdown-item>查看我的授课</el-dropdown-item>
-                <el-dropdown-item>创建一门课程</el-dropdown-item>
-              </el-dropdown-menu>
-            </el-dropdown>
+            <div class="before-login" v-if="isNotLogin">
 
-            <el-dropdown style="margin-right:20px;">
-              <span class="el-dropdown-link">
-                我的课程<i class="el-icon-arrow-down el-icon--right"></i>
-              </span>
-              <el-dropdown-menu slot="dropdown" style="margin-right:10px;">
-                <el-dropdown-item>已购课程</el-dropdown-item>
-                <el-dropdown-item>班级课程</el-dropdown-item>
-                <el-dropdown-item>收藏课程</el-dropdown-item>
-              </el-dropdown-menu>
-            </el-dropdown>
+                <el-button class="tag-style" type="text">成为讲师</el-button>
+                <el-button class="tag-style" type="text">年费会员</el-button>
+                <el-button class="tag-style" type="text" @click="loginDialog = true">微信登录</el-button>
 
-            <el-dropdown style="margin-right:10px;">
-              <el-button type="primary" icon="el-icon-service" circle style="border-radius: 40px;"></el-button>
-              <el-dropdown-menu slot="dropdown">
-                <router-link to="/userInfoCenter/userDetails" class="link-tag">
-                  <el-dropdown-item>个人信息</el-dropdown-item>
-                </router-link>
-                <router-link to="/userInfoCenter/userOrders" class="link-tag">
-                  <el-dropdown-item>我的订单</el-dropdown-item>
-                </router-link>
-                <router-link to="/userInfoCenter/studyRecord" class="link-tag">
-                  <el-dropdown-item>学习记录</el-dropdown-item>
-                </router-link>
-                <router-link to="/userInfoCenter/userCollect" class="link-tag">
-                  <el-dropdown-item>我的收藏</el-dropdown-item>
-                </router-link>
-                <el-dropdown-item>退出</el-dropdown-item>
-              </el-dropdown-menu>
-            </el-dropdown>
+                <el-dialog :visible.sync="loginDialog" width="35%" :before-close="handleClose"
+                           style="text-align: left;padding: 0px;">
+                    <el-row :gutter="12">
+                        <el-col :span="12">
+                            <el-card shadow="hover">
+                                <img src="../../../assets/img/user/QR_code.png" class="image" style="width: 300px;height: 200px;">
+                                扫描微信二维码登录
+                            </el-card>
+                        </el-col>
+                        <el-col :span="12">
+                            <el-card shadow="hover">
+                                <img src="../../../assets/img/user/QR_code.png" class="image" style="width: 300px;height: 200px;">
+                                扫描QQ二维码登录<span @click="loginDialog = false,isNotLogin = false" style="color: #28a745;">(登录成功)</span>
+                            </el-card>
+                        </el-col>
+                    </el-row>
+                </el-dialog>
+            </div>
+            <div class="after-login" v-else="isNotLogin">
+                <el-dropdown style="margin-right:20px;">
+                  <span class="el-dropdown-link">
+                    讲师<i class="el-icon-arrow-down el-icon--rights"></i>
+                  </span>
+                  <el-dropdown-menu slot="dropdown" style="margin-right:10px;">
+                    <el-dropdown-item>查看我的授课</el-dropdown-item>
+                    <el-dropdown-item>创建一门课程</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+
+                <el-dropdown style="margin-right:20px;">
+                  <span class="el-dropdown-link">
+                    我的课程<i class="el-icon-arrow-down el-icon--right"></i>
+                  </span>
+                  <el-dropdown-menu slot="dropdown" style="margin-right:10px;">
+                    <el-dropdown-item>已购课程</el-dropdown-item>
+                    <el-dropdown-item>班级课程</el-dropdown-item>
+                    <el-dropdown-item>收藏课程</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+
+                <el-dropdown style="margin-right:10px;">
+                  <el-button type="primary" icon="el-icon-service" circle style="border-radius: 40px;"></el-button>
+                  <el-dropdown-menu slot="dropdown">
+                    <router-link to="/userInfoCenter/userDetails" class="link-tag">
+                      <el-dropdown-item>个人信息</el-dropdown-item>
+                    </router-link>
+                    <router-link to="/userInfoCenter/userOrders" class="link-tag">
+                      <el-dropdown-item>我的订单</el-dropdown-item>
+                    </router-link>
+                    <router-link to="/userInfoCenter/studyRecord" class="link-tag">
+                      <el-dropdown-item>学习记录</el-dropdown-item>
+                    </router-link>
+                    <router-link to="/userInfoCenter/userCollect" class="link-tag">
+                      <el-dropdown-item>我的收藏</el-dropdown-item>
+                    </router-link>
+                    <el-dropdown-item @click.native="isNotLogin = true">退出</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
           </div>
+
         </div>
 
       </div>
@@ -86,6 +110,12 @@
 
   @Component({
     name: 'NavBar',
+    data() {
+        return {
+            isNotLogin:true,
+            loginDialog:false
+        }
+    },
     components: {
       DashDialog: () => import(/* webpackChunkName: "group-navbar" */ '@/components/home/DashDialog/DashDialog.vue'),
       CategoryIndex: () => import(/* webpackChunkName: "group-navbar" */ '@/components/course/CategoryIndex/CategoryIndex.vue'),
@@ -95,11 +125,18 @@
         isDashBoardActive: 'dashboard/isDashBoardActive',
       }),
     },
+      methods: {
+          handleClose(done) {
+              this.$confirm('确认关闭？')
+                  .then(_ => {
+                      done();
+                  })
+                  .catch(_ => {});
+          }
+      }
   })
   export default class NavBar extends Vue {
     public isDashBoardActive: boolean
-
-    public isNotLogin: boolean = true
 
     public DashDialogOptions: DashDialogOptionsInterface = {
       abstract: 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum LoreLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumm ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum',
@@ -189,4 +226,7 @@
     width: 100px;
     height: 450px;
   }
+    .tag-style {
+        margin-right:10px;
+    }
 </style>
